af4bc7db69e49e488dd223f440c554739c36cd69
par5ul1 <elexir0@gmail.com>
2023-04-01 01:33:41 -0700

    BionicWord now works with new bionicLowFixation

--- src/Components/BionicWord.jsx
+++ src/Components/BionicWord.jsx
@@ -1,13 +1,14 @@
 import { getBionicLowFixationIndex } from "../Libraries/bionicLowFixation";
 
 const BionicWord = ({ children }) => {
-  const index = getBionicLowFixationIndex(children);
+  const [startIndex, endIndex] = getBionicLowFixationIndex(children);
 
   return (
     <>
       <span>
-        <b>{children.slice(0, index)}</b>
-        {children.slice(index, children.length)}
+        {children.slice(0, startIndex)}
+        <b>{children.slice(startIndex, endIndex)}</b>
+        {children.slice(endIndex, children.length)}
       </span>
     </>
   );
